9a009d0146bccdea6b7fd6a3577efd1066c9350d
BOHverkill <BOHverkill@users.noreply.github.com>
2015-09-10 20:07:59 +0200

    sudo ist nicht nötig und systemd files hinzugefügt

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,41 +1,49 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get -y upgrade
+apt-get update
+apt-get -y upgrade
 
 # openjdk installieren
-sudo apt-get install --no-install-recommends -y openjdk-7-jdk openjdk-7-jre-headless
+apt-get install --no-install-recommends -y openjdk-7-jdk openjdk-7-jre-headless
 
 # mariadb installieren
 export DEBIAN_FRONTEND=noninteractive
 MYSQL_PASS=geheim
-sudo debconf-set-selections <<< "mariadb-server-10.0 mysql-server/root_password password $MYSQL_PASS"
-sudo debconf-set-selections <<< "mariadb-server-10.0 mysql-server/root_password_again password $MYSQL_PASS"
+debconf-set-selections <<< "mariadb-server-10.0 mysql-server/root_password password $MYSQL_PASS"
+debconf-set-selections <<< "mariadb-server-10.0 mysql-server/root_password_again password $MYSQL_PASS"
 
-sudo apt-get install -y --allow-unauthenticated mariadb-server mariadb-client
+apt-get install -y --allow-unauthenticated mariadb-server mariadb-client
+
+# nginx
+apt-get install -y nginx
 
 # jruby installieren
 JRUBY_VERSION=9.0.1.0
 wget -q https://s3.amazonaws.com/jruby.org/downloads/$JRUBY_VERSION/jruby-bin-$JRUBY_VERSION.tar.gz -P /tmp/
-sudo tar -xf /tmp/jruby-bin-$JRUBY_VERSION.tar.gz -C /opt/
+tar -xf /tmp/jruby-bin-$JRUBY_VERSION.tar.gz -C /opt/
 
-sudo cp /vagrant/bootstrap/jruby.sh /etc/profile.d/
+cp /vagrant/bootstrap/jruby.sh /etc/profile.d/
 source /etc/profile.d/jruby.sh # achtung, jruby-version hier auch anpassen
 
-sudo chown vagrant:vagrant /opt/jruby-$JRUBY_VERSION/ -R
+chown vagrant:vagrant /opt/jruby-$JRUBY_VERSION/ -R
 
 # pakete installieren
 jruby -S gem install bundler
 
 # webseite holen
 mkdir -p /var/www/holarse
-sudo chown vagrant:vagrant /var/www/holarse
+chown vagrant:vagrant /var/www/holarse
 wget -q https://github.com/Holarse-Linuxgaming/website/archive/master.zip -P /tmp
 unzip /tmp/master.zip -d /var/www/holarse
-sudo chown vagrant:vagrant /var/www/holarse/website-master -R
 
 # installieren
 cd /var/www/holarse/website-master
-jruby -S bundle install
-jruby -S rake db:setup
+chown vagrant:vagrant /var/www/holarse/website-master -R
+su vagrant -c '/opt/jruby-9.0.1.0/bin/jruby -S bundle install'
+su vagrant -c '/opt/jruby-9.0.1.0/bin/jruby -S rake db:setup'
 
-nohup jruby -S rails s -b 0.0.0.0 &
+# systemd
+cp /vagrant/holarse-www.service /lib/systemd/system/holarse-www.service
+systemctl daemon-reload
+systemctl enable holarse-www
+systemctl start holarse-www
+
